95d6bd9e3265645038a1766427cc3560378ddb08
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2024-12-06 21:49:31 -0200

    construtores

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dark", "TV", 45, "Terror", "Production IG");
+        anime.imprime();
+    }
+}
