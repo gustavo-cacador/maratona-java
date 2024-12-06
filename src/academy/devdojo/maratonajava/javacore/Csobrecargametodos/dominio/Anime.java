c836b563fe0a26a8fd2fac6b5540bfbaf49ea8ae
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2024-12-06 21:47:14 -0200

    sobrecarga de metodos

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/dominio/Anime.java
@@ -0,0 +1,60 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio;
+
+public class Anime {
+    private String nome;
+    private String tipo;
+    private int episodios;
+    private String genero;
+
+    public void init(String nome, String tipo, int episodios){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    // Sobrecarga de Métodos = mesmo nome mas com números de parâmetros diferentes
+    public void init(String nome, String tipo, int episodios, String genero){
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getEpisodios() {
+        return episodios;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+}
