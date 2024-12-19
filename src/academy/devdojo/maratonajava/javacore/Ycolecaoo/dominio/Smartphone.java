c8903bd10b56cce19c9b1f3adf3d0dceb527f35f
Gustavo Caçador <gustavoirl456@outlook.com>
2024-12-19 14:07:46 -0300

    equals pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecaoo/dominio/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecaoo/dominio/Smartphone.java
@@ -4,11 +4,22 @@ public class Smartphone {
 
     private String serialNumber;
     private String marca;
-    
+
     public Smartphone(String serialNumber, String marca) {
         this.serialNumber = serialNumber;
         this.marca = marca;
     }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && marca != null && serialNumber.equals(smartphone.serialNumber) && marca.equals(smartphone.marca); // verifica se todos os atributos são iguais ou não
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
