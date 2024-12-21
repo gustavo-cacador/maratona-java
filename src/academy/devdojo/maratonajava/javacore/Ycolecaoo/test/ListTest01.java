e2ce308c6c6bc0e37f536904369fa8f3f29677b2
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2024-12-21 16:50:38 -0200

    list pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecaoo/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecaoo/test/ListTest01.java
@@ -7,17 +7,24 @@ public class ListTest01 {
     public static void main(String[] args) {
      
         List<String>nomes = new ArrayList<>();
+        List<String>nomes2 = new ArrayList<>();
         nomes.add("Gustavo");
         nomes.add("Ronchi");
+        nomes2.add("Gustavo");
+        nomes2.add("Caçador");
+
+        nomes.addAll(nomes2);
 
         for (String nome : nomes) {
             System.out.println(nome);
         }
 
+        // Adicionando esse Objeto no 2º for
         nomes.add("Caçador");
-        
+
         System.out.println("------------");
 
+        //Poderia adiocionar: int size = nomes.size(); nomes.size() = size
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
         }
