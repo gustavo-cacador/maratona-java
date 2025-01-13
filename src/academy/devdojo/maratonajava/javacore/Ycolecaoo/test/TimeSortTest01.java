792adb2d0fde43780939c9da8527b860e4308f62
Gustavo Caçador <gustavoirl456@outlook.com>
2025-01-13 14:55:47 -0300

    sorting - comparator

--- src/academy/devdojo/maratonajava/javacore/Ycolecaoo/test/TimeSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecaoo/test/TimeSortTest01.java
@@ -2,9 +2,18 @@ package academy.devdojo.maratonajava.javacore.Ycolecaoo.test;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import academy.devdojo.maratonajava.javacore.Ycolecaoo.dominio.Time;
+
+class TimeByIdComparator implements Comparator<Time> {
+
+    @Override
+    public int compare(Time time1, Time time2) {
+        return time1.getId().compareTo(time2.getId());
+    }
+}
 
 public class TimeSortTest01 {
     public static void main(String[] args) {
@@ -19,8 +28,17 @@ public class TimeSortTest01 {
             System.out.println(time);
         }
 
+        // Ordena por ordem alfabética (nome)
         Collections.sort(times);
+        System.out.println("--------------");
+        for(Time time : times) {
+            System.out.println(time);
+        }
 
+        // Ordena por Id
+        //Collections.sort(times, new TimeByIdComparator());
+        times.sort(new TimeByIdComparator());
+        System.out.println("--------------");
         for(Time time : times) {
             System.out.println(time);
         }
