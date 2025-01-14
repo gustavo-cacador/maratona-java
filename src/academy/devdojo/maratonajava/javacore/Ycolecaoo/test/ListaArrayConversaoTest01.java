9c50b4887d9f92c403b9add442a22eb7a576b887
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-01-14 00:25:41 -0300

    lists conversao arrays

--- src/academy/devdojo/maratonajava/javacore/Ycolecaoo/test/ListaArrayConversaoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecaoo/test/ListaArrayConversaoTest01.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class ListaArrayConversaoTest01 {
     public static void main(String[] args) {
 
+        // Convertendo List para Array
         List<Integer> numeros = new ArrayList<>();
         numeros.add(1);
         numeros.add(2);
@@ -14,17 +15,21 @@ public class ListaArrayConversaoTest01 {
         Integer[] listToArray = numeros.toArray(new Integer[0]);
         System.out.println(Arrays.toString(listToArray));
 
+        System.out.println("-------------------");
+        // Convertendo Array para List
         Integer[] numerosArray = new Integer[3];
         numerosArray[0] = 1;
         numerosArray[1] = 2;
         numerosArray[2] = 3;
 
+        // Convertendo Array que criamos logo acima para List
         List<Integer> arrayToList = Arrays.asList(numerosArray);
         arrayToList.set(0, 12);
         System.out.println(Arrays.toString(numerosArray));
         System.out.println(arrayToList);
 
-        System.out.println("------------");
+        // Criando uma nova ArrayList de uma Array
+        System.out.println("-------------------");
         List<Integer> numerosList = new ArrayList<>(Arrays.asList(numerosArray));
         numerosList.add(15);
         System.out.println(numerosList);
